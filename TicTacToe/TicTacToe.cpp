91b25b6fa70e5e34a19b3b86293f9d8611ffa102
Christian F <70570052+MarioToast@users.noreply.github.com>
2020-09-22 11:31:50 +0200

    fdsaaf

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <conio.h>
+#include <windows.h>
 
 char space1 = '1';
 char space2 = '2';
@@ -10,15 +12,32 @@ char space7 = '7';
 char space8 = '8';
 char space9 = '9';
 
+bool won = false;
 
+int selection;
 
 int players;
 
 int singleplayer() {
-    
+    return 0;
 }
 
 int multiplayer() {
+    do {
+        std::cout << " | " << space1 << " | " << space2 << " | " << space3 << " |\n";
+        std::cout << " | " << space4 << " | " << space5 << " | " << space6 << " |\n";
+        std::cout << " | " << space6 << " | " << space8 << " | " << space9 << " |\n";
+        std::cout << "Select a number between 1 and 9.\n";
+        std::cin >> selection;
+        switch (selection) {
+        case '1':
+            if (space1 != 'X' && space1 != 'O') {
+                space1 = 'X';
+            }
+            break;
+        }
+    } while (won == false);
+    return 0;
 }
 
 int main()
@@ -29,6 +48,7 @@ int main()
     std::cout << " | " << space6 << " | " << space8 << " | " << space9 << " |\n";
     std::cout << "But first: one or two players?\n";
     std::cin >> players;
+    system("cls");
     switch (players) {
     case '1': case 'one':
         singleplayer();
